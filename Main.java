e9df1654fdbff5dee19b94b5d701574748a7e21e
cartierpackk <carterl8@nycstudents.net>
2025-05-07 10:10:33 -0400

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(APCalendar.numberOfLeapYears(2016, 2020));
+        System.out.println(APCalendar.dayOfWeek(1, 5, 2019));
+        System.out.println(APCalendar.dayOfWeek(1, 10, 2019));
+    }
+}
